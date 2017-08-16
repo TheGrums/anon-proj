df931dcf72bdb2f2c494b5e398072d63a7b34101
root <root@scribvs.com>
2017-08-16 16:04:44 +0200

    Slowly creating skeleton

--- serverHandler.js
+++ serverHandler.js
@@ -1,26 +1,62 @@
 
 function buildResponse(res){
-  return res.json({This:"is magic"});
+  return res.json({
+  "version": "1.0",
+  "sessionAttributes": {
+    "supportedHoriscopePeriods": {
+      "daily": true,
+      "weekly": false,
+      "monthly": false
+    }
+  },
+  "response": {
+    "outputSpeech": {
+      "type": "PlainText",
+      "text": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless. Can I help you with anything else?"
+    },
+    "shouldEndSession": false
+  }
+});
 }
 
+function startServer(){
+
+  let express = require('express'),
+      bodyParser = require('body-parser'),
+      app = express();
+      alexaVerifier = require('alexa-verifier');
+
+  function requestVerifier(req, res, next) {
+      alexaVerifier(
+          req.headers.signaturecertchainurl,
+          req.headers.signature,
+          req.rawBody,
+          function verificationCallback(err) {
+              if (err) {
+                  res.status(401).json({ message: 'Verification Failure', error: err });
+              } else {
+                  next();
+              }
+          }
+      );
+  }
+
+  app.use(bodyParser.json());
+
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  app.post('/', requestVerifier, function(req, res) {
+      buildResponse(res);
+  });
+
+  app.listen(8888);
+
+}
 
 module.exports = {
-  start : function(){
-    let express = require('express'),
-        bodyParser = require('body-parser'),
-        app = express();
-
-
-    app.use(bodyParser.json());
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
-    });
-    app.get('/', function(req, res) {
-        buildResponse(res);
-    });
-
-    app.listen(8888);
-  }
+  start : startServer
 }
