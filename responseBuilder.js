fcc8e66a86af6df7d11241a2f8682c341171d9c5
TheGrums <lologrum10@gmail.com>
2017-08-28 14:32:02 +0200

    correcting bug

--- responseBuilder.js
+++ responseBuilder.js
@@ -145,7 +145,7 @@ function filterData(data,req,cb1,cbarg,...args){
   else if(safeStationList(data,req.body.request.intent.slots.Radio.value).radios.length>1&&(typeof req.body.request.dialogState === "undefined"||req.body.request.dialogState=="STARTED")){
     var man = require('./objectsCollection');
     var msg = "This led me to several radio stations<break time='0.5s'/> could you be more specific ? Here is a sample of what I've found :";
-    safeStationList(data,req.body.request.intent.slots.Radio.value).radios.slice(0,3).forEach((a)=>{msg+=" <emphasis level='reduced'>"+a.Name+"</emphasis><break time='0.5s'/>";});
+    safeStationList(data,req.body.request.intent.slots.Radio.value).radios.slice(0,3).forEach((a)=>{msg+=("<emphasis level='reduced'>"+a.Name+"</emphasis><break time='0.5s'/>");});
     cbarg(new man.responseObject(new man.Response(false,[new man.ElicitDirective("Radio",new man.Intent(req.body.request.intent.name,{"Radio":new man.Slot("Radio")}),"Dialog.ElicitSlot")],new man.OutputSpeech(msg)))); // Executing the second callback function to respond directly
   }
   else if(typeof data.radios[0].UID==="undefined"||data.radios.UID==""){
